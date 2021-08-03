f5bea054877328918fa5e7c995d199375d30d76f
Stefan <stefan.schlombs@gmail.com>
2021-08-04 00:45:37 +0200

    Search

--- Blog.search.php
+++ Blog.search.php
@@ -2,9 +2,29 @@
 
 class ApplicationBlog_search extends ApplicationSearch_abstract
 {
-   public function getForm() {
+    public function getForm(ContainerExtensionTemplateParseCreateForm_helper $formHelper): string
+    {
+        $templateCache = new ContainerExtensionTemplateLoad_cache_template(Core::getRootClass(__CLASS__),
+                                                                           'search');
 
-   }
+        /** @var ContainerExtensionTemplate $template */
+        $template = Container::get('ContainerExtensionTemplate');
+        $template->set($templateCache->getCacheContent()['search']);
+
+        $formHelper->addFormElement('dateFrom',
+                                    'date');
+
+        $formHelper->addFormElement('dateTo',
+                                    'date');
+
+        $template->assign('date',
+                          $formHelper->getElements());
+
+        $template->parse();
+
+        return $template->get();
+
+    }
 
 
 }
---
+++ Blog.template.search.tpl
@@ -0,0 +1,10 @@
+<div class="card-container card-container--shadow">
+    <div class="card-container-header">
+        {insert/language class="ApplicationSearch" path="/form/search/search"
+        language-de_DE="Datum Filterung"
+        language-en_US="Date Filter"}
+    </div>
+    <div class="card-container-content">
+        {$date}
+    </div>
+</div>
